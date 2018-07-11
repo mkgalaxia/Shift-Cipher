325fde1276dd9b2151b34b86713deb25a1d1cfb7
Andrew Cate <andrew92508@gmail.com>
2018-07-10 17:42:54 -0700

    Adding Documentation
    adding documentation to code and cleaning the lower case letter section to match the rest.

--- shiftCipherMain.cpp
+++ shiftCipherMain.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 #include <string>
 
+//rotates uppercase and lowercase letters and numbers and leaves special characters
 char rotateChar(char myChar, int rotateNum) {
+	//rotates number characters
 	if (myChar >= 48 && myChar <= 57) {
 		myChar += (rotateNum % 10);
+		//rolls around number if rotateNum is too big
 		if (myChar > 57) {
 			myChar -= 10;
 		}
@@ -12,8 +15,10 @@ char rotateChar(char myChar, int rotateNum) {
 			myChar += 10;
 		}
 	}
+	//rotates upper case letters
 	else if (myChar >= 65 && myChar <= 90) {
 		myChar += (rotateNum % 26);
+		//rolls around upper case letters if rotateNum is to big
 		if (myChar > 90) {
 			myChar -= 26;
 		}
@@ -21,12 +26,12 @@ char rotateChar(char myChar, int rotateNum) {
 			myChar += 26;
 		}
 	}
+	//rotates lower case letters
 	else if (myChar >= 97 && myChar <= 122) {
-		if (myChar + (rotateNum % 26) > 122) {
-			myChar += (rotateNum % 26) - 26;
-		}
-		else {
-			myChar += (rotateNum % 26);
+    		myChar += (rotateNum % 26);
+		//rolls around lower case letters if rotateNum is too big
+		if (myChar > 122) {
+			myChar -= 26;
 		}
 		if (myChar < 97) {
 			myChar += 26;
@@ -45,6 +50,7 @@ int main() {
 	cout << "Please insert the number you would like to offset your message." << endl;
 	cin >> rotateNum;
 
+	//rotates each char with rotateChar and prints
 	for (int i = 0; i < mystring.length(); i++) {
 		cout << rotateChar(mystring[i], rotateNum);
 	}
